b2ca950568951058d0fed8ebc21d6cba23ed3a3c
Skxawng <lbruzond@gmail.com>
2020-01-02 23:53:48 -0500

    Reformat code.

--- src/TeleNewPlayers.cpp
+++ src/TeleNewPlayers.cpp
@@ -7,43 +7,50 @@
 #include "Config.h"
 #include "Chat.h"
 
-class TeleNewPlayers : public PlayerScript{
+class TeleNewPlayers : public PlayerScript
+{
 public:
+	TeleNewPlayers() : PlayerScript("TeleNewPlayers") {}
 
-    TeleNewPlayers() : PlayerScript("TeleNewPlayers") { }
+	void OnLogin(Player *player) override
+	{
+		if (sConfigMgr->GetBoolDefault("TeleNewPlayers.Enable", false) && sConfigMgr->GetBoolDefault("TeleNewPlayers.Announce", false))
+		{
+			ChatHandler(player->GetSession()).SendSysMessage("# Modulo |cff4CFF00TeleNewPlayers |r habilitado:\n##### Teletransporta a los nuevos jugadores al lugar escogido.");
+		}
+	}
 
-    void OnLogin(Player* player) override {
-        if (sConfigMgr->GetBoolDefault("TeleNewPlayers.Enable", false) && sConfigMgr->GetBoolDefault("TeleNewPlayers.Announce", false)) {
-            ChatHandler(player->GetSession()).SendSysMessage("# Modulo |cff4CFF00TeleNewPlayers |r habilitado:\n##### Teletransporta a los nuevos jugadores al lugar escogido.");
-        }
-    }
-	
-	void OnFirstLogin(Player* player) override {
-        if (sConfigMgr->GetBoolDefault("TeleNewPlayers.Enable", false)) {
+	void OnFirstLogin(Player *player) override
+	{
+		if (sConfigMgr->GetBoolDefault("TeleNewPlayers.Enable", false))
+		{
 			uint32 mapid = 0;
 			float posx = 0;
 			float posy = 0;
 			float posz = 0;
 			float orientation = 0;
-			if (!(player->GetTeamId() == TEAM_HORDE)){
+			if (!(player->GetTeamId() == TEAM_HORDE))
+			{
 				mapid = sConfigMgr->GetIntDefault("TeleNewPlayers.Location.Alliance.MapID", 571);
 				posx = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Alliance.PosX", 5813.83f);
 				posy = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Alliance.PosY", 448.933f);
 				posz = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Alliance.PosZ", 658.751f);
 				orientation = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Alliance.Orientation", 1.31092f);
-			}else{
+			}
+			else
+			{
 				mapid = sConfigMgr->GetIntDefault("TeleNewPlayers.Location.Horde.MapID", 571);
 				posx = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Horde.PosX", 5813.83f);
 				posy = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Horde.PosY", 448.933f);
 				posz = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Horde.PosZ", 658.751f);
 				orientation = sConfigMgr->GetFloatDefault("TeleNewPlayers.Location.Horde.Orientation", 1.31092f);
 			}
-            player->TeleportTo(mapid, posx, posy, posz, orientation);
-        }
-    }
+			player->TeleportTo(mapid, posx, posy, posz, orientation);
+		}
+	}
 };
 
-void AddTeleNewPlayersScripts() {
-    new TeleNewPlayers();
+void AddTeleNewPlayersScripts()
+{
+	new TeleNewPlayers();
 }
-
